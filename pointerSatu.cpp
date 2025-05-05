0355fd8e00a02e9acb2c1b55b113dac3b3fbff3b
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-05-05 10:45:27 +0700

    membuat reference dan dereference

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,17 @@ class mahasiswa{
         cout << "no induk = " <<nim<<endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1};//object mhs
+    mhs.showNim();//member akses operator
+
+    mahasiswa &refmhs = mhs;
+    refmhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+}
